9689247fa0031e7ada6a44491464ac12fbdd92cc
Christopher Stebe <chris@stebe.eu>
2014-02-11 14:55:39 +0100

    remove view file, use Widget class to output markup

--- components/GoogleCalendarWidget.php
+++ components/GoogleCalendarWidget.php
@@ -6,7 +6,7 @@
      * @date        2014-02-10
      * @link        https://github.com/Quexer69/yii-gcal-flow
      * @copyright   Copyright &copy; 2014 iServ4u GbR
-     * @version     1.0.0
+     * @version     2.0.0
      * @package     quexer69/yii-gcal-flow
      * @description jQuery Google Calendar Widget for Yii-Framework
      */
@@ -57,7 +57,33 @@
                     width: " . $this->width . " !important;
                 }"
             );
-            $this->render('_calendarList');
+            // Render Markup
+            echo '<div id="gcf-container">
+                    <div class="gcf-header-block">
+                        <div class="gcf-title-block">
+                            <span class="gcf-title"></span>
+                        </div>
+                    </div>
+                    <div class="gcf-item-container-block">
+                        <div class="gcf-item-block">
+                            <div class="gcf-item-header-block">
+                                <div class="gcf-item-date-block">
+                                    <span class="gcf-item-daterange"></span>
+                                </div>
+                                <div class="gcf-item-title-block">
+                                    <strong class="gcf-item-title"></strong>
+                                </div>
+                            </div>
+                            <div class="gcf-item-body-block">
+                                <div class="gcf-item-description">
+                                </div>
+                                <div class="gcf-item-location">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="gcf-last-update-block">' . Yii::t('app', 'Last update') . '&nbsp;<span class="gcf-last-update"></span></div>
+                </div>
+                <div class="clearfix"></div>';
         }
     }
-
